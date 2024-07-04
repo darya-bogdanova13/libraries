f39cea47aa4256fe31c3b4348cec733f71993814
darya <darya.kolycheva98@yandex.ru>
2024-07-04 22:53:23 +0300

    Initial commit

---
+++ src/main/java/ru/skypro/employeelibraries/EmployeelibrariesApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.employeelibraries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeelibrariesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeelibrariesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skypro/employeelibraries/EmployeelibrariesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.employeelibraries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeelibrariesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
